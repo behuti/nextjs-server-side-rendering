fe55a37c50a288b198dabc5bc812dae87bc6ea4a
behuti <echenao@gmail.com>
2023-03-15 15:58:56 -0500

    My very first getStatic Props with dummy json data

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,30 @@
-export default function Home() {
+import path from "path";
+import fs from "fs/promises";
+
+const Home = ({ products }) => {
   return (
     <>
       <main>
         <ul>
-          <li>Product 1</li>
-          <li>Product 2</li>
-          <li>Product 3</li>
+          {products.map(({ id, title }) => (
+            <li key={id}>{title}</li>
+          ))}
         </ul>
       </main>
     </>
   );
-}
+};
+
+export const getStaticProps = async () => {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      products: data.products,
+    },
+  };
+};
+
+export default Home;
